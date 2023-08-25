9e13143fa0881126eb976575908b41380cbc1f1d
Devaansh Bhandari <107868772+brf153@users.noreply.github.com>
2023-08-25 18:28:59 +0530

    changing the folder directory

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const app = express()
-require("dotenv").config({ path: "./env/config.env" })
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server working on port: ${process.env.PORT}`)
-})
