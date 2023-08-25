4170be69f5fe60aa93f3118e83d25cf2c526fefc
brf153 <153hsb@gmail.com>
2023-08-25 18:34:54 +0530

    This commit changes the directory of the project
    Co-authored-by: mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
    Signed-off-by: brf153 <153hsb@gmail.com>

--- server.js
+++ server.js
@@ -1,7 +1,59 @@
-const express = require("express")
-const app = express()
-require("dotenv").config({ path: "./env/config.env" })
+const express = require("express");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const { auth, callback } = require('./controllers/googleAuth')
+const cookieParser = require("cookie-parser");
+const {login, register, logout} = require('./controllers/authControllers')
+require("dotenv").config({path:"./env/config.env"});
+const app = express();
+const initializePassport = require('./middleware/configPassport')
+const bodyParser = require('body-parser');
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server working on port: ${process.env.PORT}`)
+const PORT = process.env.PORT || 3000;
+app.use(express.json())
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+initializePassport(passport); 
+
+app.get('/auth/google', auth)
+app.get('/auth/google/callback', callback)
+
+passport.serializeUser(function(user, done){
+  done(null, user)
 })
+
+passport.deserializeUser(function(user, done){
+  done(null, user);
+})
+
+// Parses details from a form
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+
+  })
+);
+// Funtion inside passport which initializes passport
+app.use(passport.initialize());
+// Store our variables to be persisted across the whole session. Works with app.use(Session) above
+app.use(passport.session());
+app.use(flash());
+
+app.get("/users/logout",logout)
+app.post("/users/register",register)
+app.post("/users/login",login)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
