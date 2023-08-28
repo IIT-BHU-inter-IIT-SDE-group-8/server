2c4680a8cc27a866c5944048bba2114f502afe14
brf153 <153hsb@gmail.com>
2023-08-28 14:08:45 +0530

    changed the auth controllers
    Co-authored-by: mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
    Signed-off-by: brf153 <153hsb@gmail.com>

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const passport = require("passport");
-const flash = require("express-flash");
 const session = require("express-session");
-const { auth, callback } = require('./src/controllers/googleAuth')
 const cookieParser = require("cookie-parser");
 const { login, register, logout } = require('./src/controllers/authControllers')
 require("dotenv").config();
 const app = express();
 const initializePassport = require('./src/middleware/configPassport')
 const bodyParser = require('body-parser');
+// const flash = require("express-flash");
 
+const userRouter = require('./src/routes/auth_routes');
 const PORT = process.env.PORT || 3000;
+
+//---->Setting up middleware<----//
+
+
+//Additional middlewares
 app.use(express.json())
 app.use(cookieParser());
 
@@ -18,8 +23,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 initializePassport(passport);
 
-app.get('/auth/google', auth)
-app.get('/auth/google/callback', callback)
+// app.use(flash());
+app.use(
+  session({
+
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+
+  })
+);
 
 // Passport
 passport.serializeUser(function(user, done) {
@@ -32,7 +45,10 @@ passport.deserializeUser(function(user, done) {
 
 // Parses details from a form
 app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
+
+//Router
+app.use( "/users", userRouter);
 
 
 
@@ -49,7 +65,6 @@ app.use(
 app.use(passport.initialize());
 // Store our variables to be persisted across the whole session. Works with app.use(Session) above
 app.use(passport.session());
-app.use(flash());
 
 app.get("/users/logout", logout)
 app.post("/users/register", register)
