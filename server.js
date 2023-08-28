9983d37ae7362377d7d850376eb7eacd0fd765ec
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2023-08-28 20:28:35 +0530

    added controllers, routing and model for communities and communities-trip

--- server.js
+++ server.js
@@ -1,12 +1,64 @@
-const app = require('./app.js');
-const { createUsersTable } = require("./src/models/userModel");
+const express = require("express");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const { auth, callback } = require('./src/controllers/googleAuth')
+const cookieParser = require("cookie-parser");
+const { login, register, logout } = require('./src/controllers/authControllers')
+require("dotenv").config();
+const app = express();
+const initializePassport = require('./src/middleware/configPassport')
+const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 3000;
 
-// Database Models
-createUsersTable();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+initializePassport(passport);
+
+app.get('/auth/google', auth)
+app.get('/auth/google/callback', callback)
+
+// Passport
+passport.serializeUser(function(user, done) {
+    done(null, user)
+})
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+})
+
+// Parses details from a form
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+
+
+app.use(
+    session({
+
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
+
+    })
+);
+// Funtion inside passport which initializes passport
+app.use(passport.initialize());
+// Store our variables to be persisted across the whole session. Works with app.use(Session) above
+app.use(passport.session());
+app.use(flash());
+
+app.get("/users/logout", logout)
+app.post("/users/register", register)
+app.post("/users/login", login)
+
+app.use("/communities", communityRouter);
+
+
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
 
