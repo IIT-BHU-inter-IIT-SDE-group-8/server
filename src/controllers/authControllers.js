7ed706c8c52ce8d37731ead94f938515cf178dcd
aditya <adiraiar022@gmail.com>
2023-08-30 10:08:28 +0530

    added cointrollers inside try catch statement

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -5,56 +5,58 @@ const JWT_SECRET = process.env.JWT_SECRET
 const sendTrue = require('../utils/sendTrue.js');
 const { ErrorHandler } = require("../middleware/error.js");
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
     const { email, password } = req.body;
 
-    client.query(
-        `SELECT * FROM users WHERE user_email = $1`,
-        [email],
-        (err, results) => {
-            if (err) {
-                console.error("Error:", err);
-                return res.status(500).json({ error: "Internal Server Error" });
+    try {
+        client.query(
+            `SELECT * FROM users WHERE user_email = $1`,
+            [email],
+            (err, results) => {
+                if (err) {
+                    console.error("Error:", err);
+                    return res.status(500).json({ error: "Internal Server Error" });
+                }
+    
+                if (results.rows.length > 0) {
+                    const user = results.rows[0];
+    
+                    bcrypt.compare(password, user.user_password, (err, isMatch) => {
+                        if (err) {
+                            console.error("Error:", err);
+                            return res.status(500).json({ error: "Internal Server Error" });
+                        }
+    
+                        if (isMatch) {
+                            const data = {
+                                user: {
+                                    id: user.user_id
+                                }
+                            };
+                            const authToken = jwt.sign(data, JWT_SECRET);
+    
+                            // Store the authToken in a cookie
+                            res.cookie('authToken', authToken, { httpOnly: true });
+    
+                            return res.status(200).json({ success: true, authToken: authToken });
+                        } else {
+                            return res.status(400).json({
+                                success: false,
+                                error: "Please try to login with correct credentials"
+                            });
+                        }
+                    });
+                } else {
+                    return res.status(400).json({
+                        success: false,
+                        error: "No user with that email address"
+                    });
+                }
             }
-
-            if (results.rows.length > 0) {
-                const user = results.rows[0];
-
-                bcrypt.compare(password, user.user_password, (err, isMatch) => {
-                    if (err) {
-                        console.error("Error:", err);
-                        return res.status(500).json({ error: "Internal Server Error" });
-                    }
-
-                    if (isMatch) {
-                        const data = {
-                            user: {
-                                id: user.user_id
-                            }
-                        };
-                        const authToken = jwt.sign(data, JWT_SECRET);
-
-                        // Store the authToken in a cookie
-                        res.cookie('authToken', authToken, { httpOnly: true });
-
-                        return res.status(200).json({ success: true, authToken: authToken });
-                    } else {
-                        return res.status(400).json({
-                            success: false,
-                            error: "Please try to login with correct credentials"
-                        });
-                    }
-                });
-            } else {
-                return res.status(400).json({
-                    success: false,
-                    error: "No user with that email address"
-                });
-            }
-        } else {
-            return next(new ErrorHandler("No user exists with that email address",404));
-        }
-    );
+        );
+    } catch (error) {
+        next(error);
+    }
 };
 
 
--- src/controllers/tripController.js
+++ src/controllers/tripController.js
@@ -2,12 +2,13 @@ const { client } = require('../config/configDB')
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET
 
-const createTrip = async (req, res) => {
+const createTrip = async (req, res, next) => {
     const token = req.header('auth-token');
     const { name, origin, destination, desc, departure_dateTime, arrival_dateTime } = req.body
     const data = jwt.verify(token, JWT_SECRET);
     const user_id = data.user.user_id;
-    client.query(`
+    try {
+        client.query(`
         INSERT INTO trips (trip_name, trip_origin, trip_destination, trip_desc, trip_departure_datetime, trip_arrival_datetime)
         VALUES ($1, $2, $3, $4, $5, $6)
         RETURNING trip_id`,
@@ -28,16 +29,18 @@ const createTrip = async (req, res) => {
                     if (err) {
                         throw err;
                     }
-                }
-            );
-        }
-    )
-
-    const message = "Trip created successfully";
-    res.json({ message });
+                });
+            }
+        )
+            
+        const message = "Trip created successfully";
+        res.json({ message });
+    } catch (error) {
+        next(error);
+    }
 }
 
-const UpdateTrip = async (req, res) => {
+const UpdateTrip = async (req, res, next) => {
     const { name, origin, destination, desc, departure_dateTime, arrival_dateTime } = req.body
     const trip_id = req.params.trip_id;
     try {
@@ -53,11 +56,11 @@ const UpdateTrip = async (req, res) => {
         })
         res.json({message: "trip updated successfully!"});
     } catch (error) {
-        console.log(error);
+        next(error);
     }
 }
 
-const deleteTrip = async (req, res) => {
+const deleteTrip = async (req, res, next) => {
     const trip_id = req.params.trip_id;
     try {
         client.query(`DELETE FROM trips
@@ -68,11 +71,11 @@ const deleteTrip = async (req, res) => {
         })
         res.json({message: "Trip deleted successfully."})
     } catch (error) {
-        console.log(error);
+        next(error);
     }
 }
 
-const queryTripsByCommunityId = async (req, res) => {
+const queryTripsByCommunityId = async (req, res, next) => {
     // Collect unique dates, origins, and destinations
     const tripIds = new Set();
     const community_id = req.params.community_id;
@@ -80,76 +83,80 @@ const queryTripsByCommunityId = async (req, res) => {
     const data = jwt.verify(token, JWT_SECRET);
     const user_id = data.user.id;
     
-    if(community_id !== undefined)
-    {
-        let sqlQuery = `
-        SELECT DISTINCT CT.trip_id
-        FROM community_trip CT
-        WHERE CT.community_id = ${community_id}
-        `;
-
-        // Execute the SQL query
-        client.query(sqlQuery, (err, results) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).json({ error: 'An error occurred while querying the database' });
-            }
-
-            results.rows.forEach((row) => {
-                tripIds.add(row.trip_id);
-            });
-
-            getTrips(req, res, tripIds);
-        });
-    }
-    else if(community_id === undefined){
-        
-        client.query(`WITH UserFriends AS (
-            SELECT DISTINCT user2_id AS friend_id
-            FROM friendship
-            WHERE user1_id = $1
-            UNION
-            SELECT DISTINCT user1_id AS friend_id
-            FROM friendship
-            WHERE user2_id = $1
-        )
-        
-        SELECT DISTINCT UT.trip_id
-        FROM user_trip UT
-        WHERE UT.user_id = $1
-            OR UT.user_id IN (
-                SELECT UC.user_id
-                FROM user_community UC
-                WHERE UC.community_id IN (
-                    SELECT UC2.community_id
-                    FROM user_community UC2
-                    WHERE UC2.user_id = $1
-                )
-            )
-            OR UT.user_id IN (
-                SELECT friend_id
-                FROM UserFriends
-            )
-        UNION
-        SELECT DISTINCT CT.trip_id
-        FROM community_trip CT
-        WHERE CT.community_id IN (
-            SELECT UC.community_id
-            FROM user_community UC
-            WHERE UC.user_id = $1
-        );        
-        `,[user_id],(err,results)=>{
-            if (err) {
-                // Handle the error here
-                console.error(err);
-            } else {
-                const tripIds = new Set();
-                results.rows.map(ele => {
-                    tripIds.add(ele.trip_id);
+    try {
+        if(community_id !== undefined)
+        {
+            let sqlQuery = `
+            SELECT DISTINCT CT.trip_id
+            FROM community_trip CT
+            WHERE CT.community_id = ${community_id}
+            `;
+    
+            // Execute the SQL query
+            client.query(sqlQuery, (err, results) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'An error occurred while querying the database' });
+                }
+    
+                results.rows.forEach((row) => {
+                    tripIds.add(row.trip_id);
                 });
+    
                 getTrips(req, res, tripIds);
-            }
-        })
+            });
+        }
+        else if(community_id === undefined){
+            
+            client.query(`WITH UserFriends AS (
+                SELECT DISTINCT user2_id AS friend_id
+                FROM friendship
+                WHERE user1_id = $1
+                UNION
+                SELECT DISTINCT user1_id AS friend_id
+                FROM friendship
+                WHERE user2_id = $1
+            )
+            
+            SELECT DISTINCT UT.trip_id
+            FROM user_trip UT
+            WHERE UT.user_id = $1
+                OR UT.user_id IN (
+                    SELECT UC.user_id
+                    FROM user_community UC
+                    WHERE UC.community_id IN (
+                        SELECT UC2.community_id
+                        FROM user_community UC2
+                        WHERE UC2.user_id = $1
+                    )
+                )
+                OR UT.user_id IN (
+                    SELECT friend_id
+                    FROM UserFriends
+                )
+            UNION
+            SELECT DISTINCT CT.trip_id
+            FROM community_trip CT
+            WHERE CT.community_id IN (
+                SELECT UC.community_id
+                FROM user_community UC
+                WHERE UC.user_id = $1
+            );        
+            `,[user_id],(err,results)=>{
+                if (err) {
+                    // Handle the error here
+                    console.error(err);
+                } else {
+                    const tripIds = new Set();
+                    results.rows.map(ele => {
+                        tripIds.add(ele.trip_id);
+                    });
+                    getTrips(req, res, tripIds);
+                }
+            })
+        }
+    } catch (error) {
+        next(error);
     }
 };
 
