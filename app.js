ad622d22eb635520ab24f06cb0e31cc7c32cd9e6
aditya <adiraiar022@gmail.com>
2023-08-30 09:14:33 +0530

    new commit

--- app.js
+++ app.js
@@ -2,29 +2,15 @@ const express = require("express");
 const passport = require("passport");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
-const {
-  createTrip, trip_link_to_community, link_user_to_community, link_user_to_trip, link_user_to_user
-} = require('./src/models/tripModels');
 require("dotenv").config();
 const app = express();
 const initializePassport = require('./src/middleware/configPassport')
 const bodyParser = require('body-parser');
+const path = require('path');
+const {errorMiddleware} = require('./middleware/error');
 // const flash = require("express-flash");
-const communityRouter = require('./src/routes/community_routes');
+
 const userRouter = require('./src/routes/auth_routes');
-const tripRouter = require('./src/routes/trip_routes');
-const { createUsersTable } = require("./src/models/userModel");
-const PORT = process.env.PORT;
-
-//---->Setting up middleware<----//
-
-// Database Models
-createUsersTable();
-createTrip();
-trip_link_to_community();
-link_user_to_community();
-link_user_to_trip();
-link_user_to_user();
 
 //Additional middlewares
 app.use(express.json())
@@ -35,47 +21,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(flash());
 app.use(
-  session({
-
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-
-  })
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
 );
 
 // Passport
-initializePassport(passport); 
-passport.serializeUser(function(user, done){
-  done(null, user)
+initializePassport(passport);
+passport.serializeUser(function (user, done) {
+    done(null, user)
 })
 
-passport.deserializeUser(function(user, done){
-  done(null, user);
+passport.deserializeUser(function (user, done) {
+    done(null, user);
 })
 
 // Parses details from a form
 app.use(express.urlencoded({ extended: false }));
+// app.set("view engine", "ejs");
 
 //Router
-app.use( "/", userRouter);
-app.use('/trips',tripRouter);
-app.use('/communities',communityRouter)
+app.use("/users", userRouter);
+
 
 // Funtion inside passport which initializes passport
 app.use(passport.initialize());
 // Store our variables to be persisted across the whole session. Works with app.use(Session) above
 app.use(passport.session());
 
+
 // Testing server
-app.get("/",(req,res)=>{
-  res.send("Welcome to the Flight!")
+app.get("/", (req, res) => {
+    const options = {
+        root: path.join(__dirname)
+    };
+    res.sendFile("api/output.html", options);
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.use(errorMiddleware);
 
-
-
-
+module.exports = app;
