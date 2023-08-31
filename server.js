577bc292f10c0e2fbfd8ad711790446f367f7b8b
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2023-08-31 21:30:40 +0530

    created functions for creating community, community_trips tables

--- server.js
+++ server.js
@@ -11,71 +11,16 @@ const bodyParser = require('body-parser');
 
 const userRouter = require('./src/routes/auth_routes');
 const PORT = process.env.PORT || 3000;
-
-//---->Setting up middleware<----//
-
-
-//Additional middlewares
-app.use(express.json())
-app.use(cookieParser());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-initializePassport(passport);
-
-// app.use(flash());
-app.use(
-  session({
-
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-
-  })
-);
-
-// Passport
-passport.serializeUser(function(user, done) {
-    done(null, user)
-})
-
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-})
-
-// Parses details from a form
-app.use(express.urlencoded({ extended: false }));
-// app.set("view engine", "ejs");
-
-//Router
-app.use( "/users", userRouter);
-
-
-
-app.use(
-    session({
-
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: false
-
-    })
-);
-// Funtion inside passport which initializes passport
-app.use(passport.initialize());
-// Store our variables to be persisted across the whole session. Works with app.use(Session) above
-app.use(passport.session());
-
-app.get("/users/logout", logout)
-app.post("/users/register", register)
-app.post("/users/login", login)
-
-app.use("/communities", communityRouter);
+const { createCommunitiesTable, createCommunitiesTripsTable } = require("./src/models/communityModel.js")
+// Database Models
+createUsersTable();
+createCommunitiesTable();
+createCommunitiesTripsTable();
 
 
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
 
 
