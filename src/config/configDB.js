580d287c42112a583cffdad40148f908f03985d6
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2023-08-31 21:36:18 +0530

    merged conflicts

--- src/config/configDB.js
+++ src/config/configDB.js
@@ -5,13 +5,13 @@
 
 
 // const pool = new Pool({
-    
+
 //     "user": process.env.DB_USER,
 //     "host": process.env.DB_HOST,
 //     "database": process.env.DB_DATABASE,
 //     "password": process.env.DB_PASSWORD,
 //     "port": process.env.DB_PORT
-    
+
 // })
 
 // // console.log(pool)
@@ -19,42 +19,31 @@
 // pool.on('connect', () => {
 //     console.log('Connected to the database');
 //   });
-  
+
 // pool.on('error', (err) => {
 //     console.error('Error connecting to the database:', err);
 //   });
 
 // module.exports = {pool}
 
-const {Client} = require("pg")
+const { Client } = require("pg")
 const path = require("path")
 require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 const client = new Client({
-  host: process.env.HOST,
-  user: process.env.DB_USER,
-  post: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    post: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
 
-client.connect().then(()=>{
-  console.log("Connected to Database")
+client.connect()
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch(error => {
+        console.error('Error connecting to the database:', error);
+    });
 
-  client.query(`
-    CREATE TABLE IF NOT EXISTS users (
-        user_id SERIAL PRIMARY KEY,
-        user_name VARCHAR(255) NOT NULL,
-        user_email VARCHAR(255) NOT NULL UNIQUE,
-        user_password VARCHAR(255) NOT NULL,
-        user_bio TEXT,
-        user_mobile VARCHAR(20)
-    );
-`)
-  .then(() => {
-    console.log('Table "users" created or already exists');
-  })
-  .catch(err => {
-    console.error('Error creating table:', err);
-  });
-
+module.exports = client
