0fa922e743b877ca9957c6605e81bc426667bb08
aditya <adiraiar022@gmail.com>
2023-09-03 09:50:13 +0530

    refeactored code

--- src/routes/trip_routes.js
+++ src/routes/trip_routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const { checkAuthenticated } = require('../middleware/checkAuthentication');
-const { getAlltrips, createTrip } = require('../controllers/tripController')
+const { getAlltrips, createTrip, UpdateTrip, deleteTrip } = require('../controllers/tripController')
 
 router.use(cors());
 
-router.get('/', checkAuthenticated, getAlltrips)
-router.post('/', checkAuthenticated, createTrip)
+router.get('/', checkAuthenticated, getAlltrips);
+router.post('/', checkAuthenticated, createTrip);
+router.put('/:trip_id',checkAuthenticated,UpdateTrip);
+router.delete('/:trip_id',checkAuthenticated,deleteTrip);
 
 module.exports = router;
