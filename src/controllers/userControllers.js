1f11a1cdc4583f704cec656d0a37c24b4cebae33
aditya <adiraiar022@gmail.com>
2023-09-18 18:14:28 +0530

    refactored code

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -177,6 +177,4 @@ const unlinkTripAndUser = (req, res) => {
 }
 
 
-
-
 module.exports = { getAllTripsOfUser, unlinkTripAndUser, getUserById, getAllUsers, updateUser, deleteUser, link_user_to_trip };
