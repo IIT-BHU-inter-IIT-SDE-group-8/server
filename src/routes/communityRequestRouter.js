4268f3fff6f8de7d07fd76e12e1c127e113de25f
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2023-09-20 17:20:08 +0530

    added routes for community requests and a few changes to the respective controller

---
+++ src/routes/communityRequestRouter.js
@@ -0,0 +1,21 @@
+
+const express = require("express");
+const { getAllCommunityRequestObjects, createCommunityRequestObject, getCommunityRequestObjectByRequestId, updateCommunityRequestObject, deleteCommunityRequestObjectById, updateCommunityRequestStatus } = require("../controllers/communityRequestController");
+const router = express.Router();
+
+
+router.route("/").get(getAllCommunityRequestObjects).post(createCommunityRequestObject)
+router.route("/:community_request_id").get(getCommunityRequestObjectByRequestId).delete(deleteCommunityRequestObjectById)
+    .put((req, res) => {
+        if (!req.headers.request_status) {
+            updateCommunityRequestObject(req, res)
+        }
+        else {
+            updateCommunityRequestStatus(req, res)
+        }
+    })
+
+// TODO: add the function that corresponds to getTripById here at the get endpoint of /:trip_id;
+// TODO: add error handling for when user tries to update a community that does not exist
+
+module.exports = router
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers, updateUser, deleteUser, link_user_to_trip, getUserById, getAllTripsOfUser, unlinkTripAndUser } = require('../controllers/userControllers');
-const { getAllTripJoinRequests, getTripById } = require('../controllers/tripController');
+const { getAlltrips, getAllTripJoinRequests, getTripById } = require('../controllers/tripController');
+const { getAllInviteObjectsByUserId, getAllRequestObjectsByAdminId } = require('../controllers/communityRequestController');
 
 router.get('/', getAllUsers);
 router.route("/:user_id").get(getUserById).put(updateUser).delete(deleteUser);
@@ -10,5 +11,14 @@ router.route("/:user_id").get(getUserById).put(updateUser).delete(deleteUser);
 router.route("/:user_id/trips").get(getAllTripsOfUser)
 router.route('/:user_id/trips/:trip_id').get(getTripById).post(link_user_to_trip).delete(unlinkTripAndUser);
 router.get('/:user_id/trips/:trip_id/join_requests', getAllTripJoinRequests)
+
+router.get("/:user_id/community_requests").get((req, res) => {
+    if (req.headers.request_type == "invite") {
+        getAllInviteObjectsByUserId(req, res)
+    }
+    else if (req.headers.request_type == "request") {
+        getAllRequestObjectsByAdminId(req, res)
+    }
+})
 
 module.exports = router;
