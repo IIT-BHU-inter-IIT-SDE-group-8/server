64d6a60b6b2681ecdc4c14013f937a914282a0e6
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2023-09-20 17:20:08 +0530

    added routes for community requests and a few changes to the respective controller

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers, updateUser, deleteUser, link_user_to_trip, getUserById, getAllTripsOfUser, unlinkTripAndUser } = require('../controllers/userControllers');
-const { getAllTripJoinRequests, getTripById } = require('../controllers/tripController');
+const { getAlltrips, getAllTripJoinRequests, getTripById } = require('../controllers/tripController');
+const { getAllInviteObjectsByUserId, getAllRequestObjectsByAdminId } = require('../controllers/communityRequestController');
 
 router.get('/', getAllUsers);
 router.route("/:user_id").get(getUserById).put(updateUser).delete(deleteUser);
@@ -11,7 +12,7 @@ router.route("/:user_id/trips").get(getAllTripsOfUser)
 router.route('/:user_id/trips/:trip_id').get(getTripById).post(link_user_to_trip).delete(unlinkTripAndUser);
 router.get('/:user_id/trips/:trip_id/join_requests', getAllTripJoinRequests)
 
-router.route("/:user_id/community_requests").get((req, res) => {
+router.get("/:user_id/community_requests").get((req, res) => {
     if (req.headers.request_type == "invite") {
         getAllInviteObjectsByUserId(req, res)
     }
