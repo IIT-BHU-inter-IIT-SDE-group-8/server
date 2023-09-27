2a2c2a68b7e64ccab7401788ed6ed871799dcd33
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2023-09-27 16:29:08 +0530

    added try catch for sendSecurityNotif

--- src/services/pushNotifications.js
+++ src/services/pushNotifications.js
@@ -15,50 +15,54 @@ webpush.setVapidDetails(
 
 //TODO: high time complexity, optimizations are welcome
 async function sendSecurityNotif(userId, message) {
-    const allEntries = new Set()
-    const friends = await getAllFriendsSubscriptions(userId)
-    friends.forEach((object) => {
-        allEntries.add(object)
-    })
-    const communities = await getAllCommunitiesOfUser(userId)
-    communities.forEach(async (communityId) => {
-        const communitySubscriptions = await getAllCommunityMembersSubscription(communityId)
-        communitySubscriptions.forEach((object) => {
+    try {
+        const allEntries = new Set()
+        const friends = await getAllFriendsSubscriptions(userId)
+        friends.forEach((object) => {
             allEntries.add(object)
         })
-    })
-    sendNotification(allEntries, "Security Alert", message)
-
-}
-
-async function notifyFriends(userId, title, message) {
-    console.log(userId)
-    try {
-        notifObjects = await getAllFriendsSubscriptions(userId)
-        console.log(notifObjects)
-        await sendNotification(notifObjects, title, message)
+        const communities = await getAllCommunitiesOfUser(userId)
+        communities.forEach(async (communityId) => {
+            const communitySubscriptions = await getAllCommunityMembersSubscription(communityId)
+            communitySubscriptions.forEach((object) => {
+                allEntries.add(object)
+            })
+        })
+        sendNotification(allEntries, "Security Alert", message)
     }
     catch (err) {
-        console.log("error while sending notifications to friends: " + err)
+        console.log("Error occurred while sending security notification: " + err)
+
     }
-}
 
-async function notifyCommunityMembers(communityId, title, message) {
-    try {
-        const notifObjects = await getAllCommunityMembersSubscription(communityId)
-        sendNotification(notifObjects, title, message)
+    async function notifyFriends(userId, title, message) {
+        console.log(userId)
+        try {
+            notifObjects = await getAllFriendsSubscriptions(userId)
+            console.log(notifObjects)
+            await sendNotification(notifObjects, title, message)
+        }
+        catch (err) {
+            console.log("error while sending notifications to friends: " + err)
+        }
     }
-    catch {
-        console.log("Error occurred while notifying all community members" + err)
+
+    async function notifyCommunityMembers(communityId, title, message) {
+        try {
+            const notifObjects = await getAllCommunityMembersSubscription(communityId)
+            sendNotification(notifObjects, title, message)
+        }
+        catch {
+            console.log("Error occurred while notifying all community members" + err)
+        }
     }
-}
 
 
-async function getAllFriendsSubscriptions(userId) {
+    async function getAllFriendsSubscriptions(userId) {
 
-    try {
-        const friends = new Set()
-        const friendsQuery = `
+        try {
+            const friends = new Set()
+            const friendsQuery = `
     SELECT
     CASE
     WHEN user1_id = $1 THEN user2_id
@@ -67,80 +71,80 @@ async function getAllFriendsSubscriptions(userId) {
     FROM friendship
     WHERE user1_id = $1 OR user2_id = $1;
 `
-        const communityUsers = await client.query(friendsQuery, [userId]);
-        communityUsers.rows.forEach((friend) => {
-            friends.add(friend.friend_id)
-        });
+            const communityUsers = await client.query(friendsQuery, [userId]);
+            communityUsers.rows.forEach((friend) => {
+                friends.add(friend.friend_id)
+            });
 
 
-        const notifObjects = await client.query(`SELECT n.*
+            const notifObjects = await client.query(`SELECT n.*
       FROM notifs n
       JOIN user_notif un ON n.notif_id = un.notif_id
       WHERE un.user_id = ANY($1::int[]);
 `, [[...friends]])
 
-        return notifObjects
+            return notifObjects
+        }
+        catch (err) {
+            console.log("Error occurred while fetching the subscription ids of all the friends of user: " + err)
+
+        }
     }
-    catch (err) {
-        console.log("Error occurred while fetching the subscription ids of all the friends of user: " + err)
 
-    }
-}
+    async function getAllCommunityMembersSubscription(communityId) {
 
-async function getAllCommunityMembersSubscription(communityId) {
-
-    try {
-        const friends = new Set()
-        const communityQuery = `
+        try {
+            const friends = new Set()
+            const communityQuery = `
     SELECT
     user_id FROM community_users
     WHERE community_id = $1;
 `
-        const communityUsers = await client.query(communityQuery, [communityId]);
-        communityUsers.rows.forEach((row) => {
-            friends.add(row.user_id)
-        });
+            const communityUsers = await client.query(communityQuery, [communityId]);
+            communityUsers.rows.forEach((row) => {
+                friends.add(row.user_id)
+            });
 
 
-        const notifObjects = await client.query(`SELECT n.*
+            const notifObjects = await client.query(`SELECT n.*
       FROM notifs n
       JOIN user_notif un ON n.notif_id = un.notif_id
       WHERE un.user_id = ANY($1::int[]);
 `, [[...friends]])
 
-        return notifObjects
-    }
-    catch (err) {
-        console.log("Error while fetching subscription ids of all the community members of user: " + err)
-    }
+            return notifObjects
+        }
+        catch (err) {
+            console.log("Error while fetching subscription ids of all the community members of user: " + err)
+        }
 
 
-}
-async function getAllCommunitiesOfUser(userId) {
-    try {
-        const communities = new Set()
-        const communityIds = await client.query("SELECT community_id from community_users WHERE user_id = $1", [userId])
-        communityIds.forEach((row) => {
-            communities.add(row.community_id)
-        })
-        return communities
     }
-    catch (err) {
-        console.log("Error occurred while getting all community ids of user: " + err)
+    async function getAllCommunitiesOfUser(userId) {
+        try {
+            const communities = new Set()
+            const communityIds = await client.query("SELECT community_id from community_users WHERE user_id = $1", [userId])
+            communityIds.forEach((row) => {
+                communities.add(row.community_id)
+            })
+            return communities
+        }
+        catch (err) {
+            console.log("Error occurred while getting all community ids of user: " + err)
+        }
     }
-}
-async function sendNotification(notifObjects, title, message) {
-    try {
-        notifObjects.rows.map((result) => {
-            let sub = { endpoint: result.endpoint, expiration_time: result.expiration_time, keys: { auth: result.auth, p256dh: result.p256dh } }
-            webpush.sendNotification(sub, `{ "title": ${title}, "message": ${message} }`);
-        })
+    async function sendNotification(notifObjects, title, message) {
+        try {
+            notifObjects.rows.map((result) => {
+                let sub = { endpoint: result.endpoint, expiration_time: result.expiration_time, keys: { auth: result.auth, p256dh: result.p256dh } }
+                webpush.sendNotification(sub, `{ "title": ${title}, "message": ${message} }`);
+            })
+        }
+        catch (err) {
+            console.log(err)
+        }
     }
-    catch (err) {
-        console.log(err)
-    }
-}
-module.exports = { sendSecurityNotif, notifyFriends, notifyCommunityMembers }
+    module.exports = { sendSecurityNotif, notifyFriends, notifyCommunityMembers }
 
 
 //var pushSubscriptionObject = {}
